5ca80ffce4ffae5abda5f0a5f024d76de4b16901
carlos <hernandez1989carlos@gmail.com>
2021-02-24 13:49:05 -0500

    comments were added to the code

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { Tasks } = require("./controller");
+const { read, save } = require("./db/store");
 const {
   mainMenu,
   pausa,
@@ -6,8 +8,6 @@ const {
   listDeleteTask,
   confirmation,
 } = require("./helpers");
-const { Tasks } = require("./controller");
-const { read, save } = require("./db/store");
 
 console.clear();
 
@@ -17,46 +17,54 @@ const main = async () => {
 
   const tasksFromDb = read();
 
+  // condicional para cargar tareas
   if (tasksFromDb) {
+    //entonces
     tasks.getDataFromStore(tasksFromDb);
   }
 
   do {
+    //imprimir menu
     option = await mainMenu();
     switch (option) {
+      //crear tarea
       case "1":
         const description = await readInput("Description:");
         tasks.createTask(description);
-
         break;
+
+      //litar todas las tareas
       case "2":
         console.log(tasks.getTaskList());
-
         break;
+
+      //listar tareas completadas
       case "3":
         console.log(tasks.listCompletePendingTask(true));
-
         break;
+
+      // listar tareas pendientes
       case "4":
         console.log(tasks.listCompletePendingTask(false));
-
         break;
+
+      // cambiar estado de tareas
       case "5":
         const ids = await showCheckList(tasks.arrayList);
         tasks.toggleStatus(ids);
         console.log("Estatus actualizado");
-
         break;
+
+      //eliminar tareas
       case "6":
         const id = await listDeleteTask(tasks.arrayList);
         if (id !== "0") {
           const confirm = await confirmation("Confirmas eliminar tarea?");
-          if (confirm) {
+          if (confirmation) {
             tasks.deleteTask(id);
             console.log("Tarea borrada");
           }
         }
-
         break;
     }
 
